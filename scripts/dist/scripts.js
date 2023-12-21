248ce87d3d03e9fcb6182fbc72161c0334ee7227
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-12-21 16:30:07 +0100

    a

--- scripts/dist/scripts.js
+++ scripts/dist/scripts.js
@@ -1,11 +1,21 @@
+// EXERCISES i TS
+// - Fixa alla document.getElementById - hint as HTMLElement, HTMLInputElement
+// - skapa en konstant variabel freight som  har värdet 200
+//          ska adderas i calculate
+// - Fixa funktionen calculateTotalPrice så den tar parametertyper och returntyp
+
+// - Skapa en klass HockeyPlayer. Den ska ha id, name, jerseyNumber, teamName
+// - Skapa en array med players
+// - loopa igenom alla och gör consolog.log
+
+
+
 const wish = document.getElementById("wish");
 const lightrope = document.getElementById("lightrope");
 const hunden = document.getElementById("hunden");
 const antal = document.getElementById("antal");
 const price = document.getElementById("price");
 const totalPrice = document.getElementById("totalPrice");
-
-const freight  = 200
 
 function calculateTotalPrice(antal, price){
     totalPrice.innerText = antal + price
@@ -24,7 +34,7 @@ function onBlurWish() {
     lightrope.style.display = "none";
 };
 function onInputWish() {
-    const inmatningen = wish === null || wish === void 0 ? void 0 : wish.value.toLowerCase();
+    const inmatningen = wish.value.toLowerCase();
     if (inmatningen.includes("hund")) {
         hunden.style.display = "block";
     }
